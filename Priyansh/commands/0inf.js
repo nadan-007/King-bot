f8c285a84625cc492ff6c59a84304a183f375fc6
nadan-007 <shahabjani741@gmail.com>
2025-05-09 02:29:13 +0300

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "info",
 	version: "1.0.1", 
-	hasPermssion: 0,
+	hasPermssion: 2,
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Admin and Bot info.",
 	commandCategory: "...",
